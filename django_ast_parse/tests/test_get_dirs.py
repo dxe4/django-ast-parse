398f08306a64d3d6f9cb1c799be9282997ab9447
charalampos papaloizou <papaloizouc@gmail.com>
2014-07-27 18:50:27 +0100

    rename func

--- django_ast_parse/tests/test_get_dirs.py
+++ django_ast_parse/tests/test_get_dirs.py
@@ -4,5 +4,6 @@ from django_ast_parse import get_files
 
 class TestGetFiles(unittest.TestCase):
 
-    def test_shuffle(self):
+    def test_get_files(self):
+        # Thats all for now..
         assert list(get_files()) > 100
