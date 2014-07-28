b0843d17947d8b2b21505bf83fb587b5802a4a42
charalampos papaloizou <papaloizouc@gmail.com>
2014-07-28 12:35:10 +0100

    fix modules view

--- ast_parse/parse/tests/test_modules.py
+++ ast_parse/parse/tests/test_modules.py
@@ -7,15 +7,23 @@ class TestModules(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_all_modules_json(self):
+    # def test_all_files(self):
+    #     response = self.client.get('/modules/all/json')
+
+    #     assert response.status_code == 200
+
+    #     data = json.loads(response.content)
+    #     files = data['files']
+    #     for _dir, _files in files.items():
+    #         for _file in _files:
+    #             assert _file.endswith('.py')
+
+    #     assert len(files.keys()) > 10
+
+    def test_all_modules(self):
         response = self.client.get('/modules/all/json')
-
         assert response.status_code == 200
+        modules = json.loads(response.content)['modules']
 
-        data = json.loads(response.content)
-        files = data['files']
-        for _dir, _files in files.items():
-            for _file in _files:
-                assert _file.endswith('.py')
-
-        assert len(files.keys()) > 10
+        assert "/django/http" in modules
+        assert len(modules) > 10
