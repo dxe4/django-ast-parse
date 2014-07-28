e7cd131926753373360259638845251f5c100af0
charalampos papaloizou <papaloizouc@gmail.com>
2014-07-28 13:45:05 +0100

    re-using code

--- ast_parse/parse/views.py
+++ ast_parse/parse/views.py
@@ -4,31 +4,44 @@ TODO refactor is_json to output to allow xml?
 import json
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-# from django.views.generic import View
+from django.views.generic import View
 from ast_parse.parse.models import NotFound
 from ast_parse.parse.utils import get_files
 
 
-def all_modules(requset, is_json=None):
-    code_base = get_files()
-    data = dict(modules=code_base.sub_dirs)
+class JsonOrHtmlView(View):
 
-    if is_json:
-        return HttpResponse(json.dumps(data),
-                            content_type="application/json")
-    else:
-        return render(requset, 'all_modules.html', data)
+    def _response(self, request, context, is_json):
+        if is_json:
+            return HttpResponse(json.dumps(context),
+                                content_type="application/json")
+        else:
+            return render(request, 'all_modules.html', context)
 
 
-def module(request, path, is_json=None):
-    '''
-    path: example -> django/http
-    '''
-    code_base = get_files()
-    try:
-        files = code_base.list_files(path)
-        print(files)
-    except NotFound:
-        raise Http404('Not found {}'.format(path))
+class AllModulesView(JsonOrHtmlView):
+    template_name = 'all_modules.html'
 
-    return render(request, 'module.html', {})
+    def get(self, request, is_json=None):
+        code_base = get_files()
+        data = dict(modules=code_base.sub_dirs)
+        return self._response(request, data, is_json)
+
+
+class ModuleView(JsonOrHtmlView):
+    template_name = 'module.html'
+
+    def get(self, request, path, is_json=None):
+        '''
+        path: example -> django/http
+        '''
+        code_base = get_files()
+        try:
+            files = code_base.list_files(path)
+        except NotFound:
+            raise Http404('Not found {}'.format(path))
+
+        return self._response(request, {}, is_json)
+
+all_modules = AllModulesView.as_view()
+module = ModuleView.as_view()
